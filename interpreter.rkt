255fdea11f685803fd7945ab45aabd73e7e32b22
Sibi <sxs1552@case.edu>
2018-02-18 23:31:45 -0500

    assign

--- interpreter.rkt
+++ interpreter.rkt
@@ -4,8 +4,8 @@
 (load "simpleParser.scm")
 
 (define evaluateDoc
-  (lambda
-      (evaluateStatements (parse "test.txt") (list '() '()) (lambda (v) v))))
+  (lambda (fileName)
+      (evaluateStatements (parse fileName) (list '() '()) (lambda (v) v))))
 
 (define evaluateStatements
   (lambda (stmts state return)
@@ -16,7 +16,7 @@
       ((eq? 'while (car stmts)) (evaluateWhile stmts state return))
       ((eq? 'return (car stmts)) (return (evaluateExpression (returnValue stmts) state)))
       ((eq? '= (car stmts)) (evaluateAssign stmts state return))
-      ((eq? 'var (car stmts)) (evaluateDeclare stmts state return))
+      ((eq? 'var (car stmts)) (evaluateDeclare stmts state))
       (else (evaluateExpression lis state)))))
 
 (define evaluateIf
@@ -31,9 +31,35 @@
         (evaluateWhile stmt (evluateStatements (ifTrue stmt) state return) return))))
 
 (define evaluateDeclare
-  (lambda (stmt state return)
-    (cons (variableName stmt) (variableList state)
- 
+  (lambda (stmt state)
+    (putInState (variableName stmt) 'error state)))
+
+(define evaluateAssign
+  (lambda (stmt state )
+    (if (isInState (variableName stmt) state)
+        (putInState (variableName stmt) (variableValue stmt) state)
+        (error 'depis))))
+
+(define isInState
+  (lambda (name state)
+    (cond
+      ((null? (variableList state)) #f)
+      ((eq? name (car (variableList state))) #t)
+      (else (isInState name (list (cdr (variableList state)) (cdr (valueList state))))))))
+
+(define putInState
+  (lambda (name val state)
+    (list (cons name (variableList (removeFromState name state))) (cons val (valueList (removeFromState name state))))))
+
+(define removeFromState
+  (lambda (name state)
+    (cond
+      ((null? (variableList state)) state)
+      ((eq? name (car (variableList state))) (list (cdr (variableList state)) (cdr (valueList state))))
+      (else (list (cons (car (variableList state)) (variableList (removeFromState name (list (cdr (variableList state)) (cdr (valueList state))))))
+                  (cons (car (valueList state)) (valueList (removeFromState name (list (cdr (variableList state)) (cdr (valueList state)))))))))))
+
+
 
 ; evaluateBool
 ; evaluateValue
@@ -58,4 +84,8 @@
 
 (define variableName cadr)
 
+(define variableValue caddr)
+
 (define variableList car)
+
+(define valueList cadr)
